43f09174f8f4ae2f011e640c427067ed813f0e4e
Tommaso Barbugli <tbarbugli@gmail.com>
2023-03-29 14:26:52 +0200

    docs

---
+++ docs/dist/swagger-initializer_chat.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "openapi/chat-openapi.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+};
