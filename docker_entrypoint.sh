666d22125ff0fb791e3a8883ab213859322fb3dd
Federico Guerinoni <federico.guerinoni@getstream.io>
2023-06-01 17:39:05 +0200

    Allow generations of SDK from a specific branch of `protocol`
    Signed-off-by: Federico Guerinoni <federico.guerinoni@getstream.io>

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,14 @@
 
 set -euo pipefail
 
-exec /home/generate.sh "$@"
+BRANCH="main"
+if [ $# -eq 3 ]; then
+  BRANCH=$(echo "$3" | sed -E 's/.*--branch=([^[:space:]]+).*/\1/')
+fi
+
+echo "branch used -> $BRANCH"
+
+git clone https://github.com/GetStream/protocol --branch ${BRANCH} /home/protocol
+
+cd /home/protocol
+exec /home/protocol/generate.sh $1 $2
