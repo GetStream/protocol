32b24ec4db33377adf7f52f42956694449b28e94
Federico Guerinoni <federico.guerinoni@getstream.io>
2023-08-23 10:13:28 +0200

    Read components of schema
    Signed-off-by: Federico Guerinoni <federico.guerinoni@getstream.io>

--- openapi-gen/openapi.go
+++ openapi-gen/openapi.go
@@ -1,11 +1,39 @@
-package openapigen
+package main
 
-import "github.com/getkin/kin-openapi/openapi3"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
 
-func ParseOpenAPISpec(filepath string) (*openapi3.T, error) {
-	return openapi3.NewLoader().LoadFromFile(filepath)
-}
+	"github.com/getkin/kin-openapi/openapi3"
+)
 
 func GetTemplates(language string) (string, error) {
 	return "", nil
+}
+
+// go run . -i ../openapi/video-openapi.yaml
+func main() {
+	inputFile := flag.String("i", "", "yaml file to use for generating code")
+	flag.Parse()
+
+	if *inputFile == "" {
+		fmt.Println("must provide input file")
+		os.Exit(1)
+	}
+
+	doc, err := openapi3.NewLoader().LoadFromFile(*inputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	err = doc.Validate(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for name, schema := range doc.Components.Schemas {
+		fmt.Println(name, schema.Value.Type, schema.Value.Format)
+	}
 }
