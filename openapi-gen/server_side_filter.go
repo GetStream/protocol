89b983ee0a9556db911a68379abc6f645b18666c
Vladislav Gruchik <4280527+vagruchi@users.noreply.github.com>
2023-10-18 15:38:50 +0200

    delete server side models and properties in schema

--- openapi-gen/server_side_filter.go
+++ openapi-gen/server_side_filter.go
@@ -17,7 +17,9 @@ var allHTTPMethods = []string{
 // removeServerSide removes the server side properties and operations from spec.
 func removeServerSide(spec *openapi3.T) {
 	spec.Paths = removeServerSideOperations(spec.Paths)
-
+	if spec.Components != nil {
+		spec.Components.Schemas = removeServerSideModels(spec.Components.Schemas)
+	}
 }
 
 func nulifyOperatioin(item *openapi3.PathItem, method string) {
@@ -47,7 +49,21 @@ func removeServerSideOperations(paths openapi3.Paths) openapi3.Paths {
 	for k, item := range paths {
 		for _, method := range allHTTPMethods {
 			op := item.GetOperation(method)
-			if op == nil || op.Extensions == nil || op.Extensions[ServerSideKey] == nil {
+			if op == nil {
+				continue
+			}
+
+			if op.Parameters != nil {
+				newParams := make(openapi3.Parameters, 0, len(op.Parameters))
+				for _, param := range op.Parameters {
+					if param.Value == nil || param.Value.Extensions == nil || param.Value.Extensions[ServerSideKey] == nil {
+						newParams = append(newParams, param)
+					}
+				}
+				op.Parameters = newParams
+			}
+
+			if op.Extensions == nil || op.Extensions[ServerSideKey] == nil {
 				continue
 			}
 			serverSide, ok := op.Extensions[ServerSideKey].(bool)
@@ -61,4 +77,46 @@ func removeServerSideOperations(paths openapi3.Paths) openapi3.Paths {
 		}
 	}
 	return paths
+}
+
+func removeServerSideModels(schemas openapi3.Schemas) openapi3.Schemas {
+	for k, item := range schemas {
+		if item == nil || item.Value == nil {
+			continue
+		}
+		removeServerSideProperties(item)
+
+		if item.Value.Extensions == nil || item.Value.Extensions[ServerSideKey] == nil {
+			continue
+		}
+
+		serverSide, ok := item.Value.Extensions[ServerSideKey].(bool)
+		if !ok || !serverSide {
+			continue
+		}
+		delete(schemas, k)
+	}
+	return schemas
+}
+
+func removeServerSideProperties(schema *openapi3.SchemaRef) {
+	if schema.Value == nil || schema.Value.Properties == nil {
+		return
+	}
+	for k, item := range schema.Value.Properties {
+		if item.Value == nil {
+			continue
+		}
+
+		removeServerSideProperties(item)
+		if item.Value.Extensions == nil || item.Value.Extensions[ServerSideKey] == nil {
+			continue
+		}
+
+		serverSide, ok := item.Value.Extensions[ServerSideKey].(bool)
+		if !ok || !serverSide {
+			continue
+		}
+		delete(schema.Value.Properties, k)
+	}
 }
--- openapi-gen/server_side_filter_test.go
+++ openapi-gen/server_side_filter_test.go
@@ -16,7 +16,7 @@ func Test_removeServerSide(t *testing.T) {
 		expect func(*testing.T, *openapi3.T)
 	}{
 		{
-			name: "removeServerSide",
+			name: "cleanOperations",
 			args: args{
 				spec: &openapi3.T{
 					Paths: openapi3.Paths{
@@ -54,6 +54,88 @@ func Test_removeServerSide(t *testing.T) {
 				}
 				if len(spec.Paths["/test"].Get.Parameters) != 0 {
 					t.Error("removeServerSide does not delete server side parameters")
+				}
+			},
+		},
+		{
+			name: "cleanSchemas",
+			args: args{
+				spec: &openapi3.T{
+					Components: &openapi3.Components{
+						Schemas: map[string]*openapi3.SchemaRef{
+							"test": {
+								Value: &openapi3.Schema{
+									Extensions: map[string]interface{}{
+										ServerSideKey: true,
+									},
+								},
+							},
+							"TestProperty": {
+								Value: &openapi3.Schema{
+									Properties: map[string]*openapi3.SchemaRef{
+										"server": {
+											Value: &openapi3.Schema{
+												Type: "string",
+												Extensions: map[string]interface{}{
+													ServerSideKey: true,
+												},
+											},
+										},
+										"client": {
+											Value: &openapi3.Schema{
+												Type: "string",
+											},
+										},
+										"Deep": {
+											Value: &openapi3.Schema{
+												Type: "object",
+												Properties: map[string]*openapi3.SchemaRef{
+													"server": {
+														Value: &openapi3.Schema{
+															Type: "string",
+															Extensions: map[string]interface{}{
+																ServerSideKey: true,
+															},
+														},
+													},
+													"client": {
+														Value: &openapi3.Schema{
+															Type: "string",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expect: func(t *testing.T, spec *openapi3.T) {
+				if _, ok := spec.Components.Schemas["test"]; ok {
+					t.Error("removeServerSide does not delete server side schema")
+				}
+				if _, ok := spec.Components.Schemas["TestProperty"]; !ok || spec.Components.Schemas["TestProperty"] == nil || spec.Components.Schemas["TestProperty"].Value == nil {
+					t.Error("removeServerSide delete schema without server side")
+				}
+				v := spec.Components.Schemas["TestProperty"].Value
+				if _, ok := v.Properties["server"]; ok {
+					t.Error("removeServerSide does not delete server side property")
+				}
+				if _, ok := v.Properties["client"]; !ok || v.Properties["client"] == nil || v.Properties["client"].Value == nil {
+					t.Error("removeServerSide delete property without server side")
+				}
+				if _, ok := v.Properties["Deep"]; !ok || v.Properties["Deep"] == nil || v.Properties["Deep"].Value == nil {
+					t.Error("removeServerSide delete property without server side")
+				}
+				dv := v.Properties["Deep"].Value
+				if _, ok := dv.Properties["server"]; ok {
+					t.Error("removeServerSide does not delete server side deep property")
+				}
+				if _, ok := dv.Properties["client"]; !ok || dv.Properties["client"] == nil || dv.Properties["client"].Value == nil {
+					t.Error("removeServerSide delete deep property without server side")
 				}
 			},
 		},
