f87150408c2e424dd16119ea064d179eea5eb3a0
Vladislav Gruchik <4280527+vagruchi@users.noreply.github.com>
2024-04-10 12:06:20 +0200

    set env in tools/install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TOOLS="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROTOC_DIR=${PROTOC_DIR:-"/opt/.protoc"}
+GOBIN=$PROTOC_DIR/bin
+
 (
   cd "$TOOLS" || exit 1
   go install ./protoc-gen-dart-twirp
