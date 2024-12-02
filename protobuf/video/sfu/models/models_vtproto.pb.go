aca8a1b4a565fe325bc164f5740a261e95124deb
thesyncim <thesyncim@gmail.com>
2024-12-02 11:44:30 +0000

    better comments

--- protobuf/video/sfu/models/models_vtproto.pb.go
+++ protobuf/video/sfu/models/models_vtproto.pb.go
@@ -558,6 +558,11 @@ func (m *PublishOption) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if m.Id != 0 {
+		i = encodeVarint(dAtA, i, uint64(m.Id))
+		i--
+		dAtA[i] = 0x40
+	}
 	if m.VideoDimension != nil {
 		size, err := m.VideoDimension.MarshalToSizedBufferVT(dAtA[:i])
 		if err != nil {
@@ -1766,6 +1771,9 @@ func (m *PublishOption) SizeVT() (n int) {
 	if m.VideoDimension != nil {
 		l = m.VideoDimension.SizeVT()
 		n += 1 + l + sov(uint64(l))
+	}
+	if m.Id != 0 {
+		n += 1 + sov(uint64(m.Id))
 	}
 	if m.unknownFields != nil {
 		n += len(m.unknownFields)
@@ -3590,6 +3598,25 @@ func (m *PublishOption) UnmarshalVT(dAtA []byte) error {
 				return err
 			}
 			iNdEx = postIndex
+		case 8:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
+			}
+			m.Id = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Id |= int32(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := skip(dAtA[iNdEx:])
