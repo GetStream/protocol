e811b2b338babd94257dd85efc3faaeca9a69b55
Zita Szupera <szuperaz@gmail.com>
2025-02-28 15:01:46 +0100

    Add moderation API spec to swagger UI

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -1,24 +1,23 @@
-window.onload = function() {
+window.onload = function () {
   window.ui = SwaggerUIBundle({
     urls: [
       {
-        name: "Chat",
-        url:"https://raw.githubusercontent.com/GetStream/protocol/main/openapi/chat-openapi.yaml"
+        name: 'Chat',
+        url: 'https://raw.githubusercontent.com/GetStream/protocol/main/openapi/chat-openapi.yaml',
       },
       {
-        name: "Video",
-        url:"https://raw.githubusercontent.com/GetStream/protocol/main/openapi/video-openapi.yaml"
-      }
+        name: 'Video',
+        url: 'https://raw.githubusercontent.com/GetStream/protocol/main/openapi/video-openapi.yaml',
+      },
+      {
+        name: 'Moderation',
+        url: 'https://raw.githubusercontent.com/GetStream/protocol/main/openapi/moderation-serverside-api.yaml',
+      },
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl,
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: 'StandaloneLayout',
   });
 };
