3a5b1844cf7f6f7704c5b81c1fff0eb1545caead
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-04-17 12:25:35 +0200

    fix: switch to float for averaged values

--- protobuf/video/sfu/models/models_vtproto.pb.go
+++ protobuf/video/sfu/models/models_vtproto.pb.go
@@ -1686,9 +1686,10 @@ func (m *EncodeStats) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		copy(dAtA[i:], m.unknownFields)
 	}
 	if m.AvgFps != 0 {
-		i = encodeVarint(dAtA, i, uint64(m.AvgFps))
+		i -= 4
+		binary.LittleEndian.PutUint32(dAtA[i:], uint32(math.Float32bits(float32(m.AvgFps))))
 		i--
-		dAtA[i] = 0x20
+		dAtA[i] = 0x25
 	}
 	if m.AvgFrameEncodeTimeMs != 0 {
 		i -= 4
@@ -2509,7 +2510,7 @@ func (m *EncodeStats) SizeVT() (n int) {
 		n += 5
 	}
 	if m.AvgFps != 0 {
-		n += 1 + sov(uint64(m.AvgFps))
+		n += 5
 	}
 	if m.unknownFields != nil {
 		n += len(m.unknownFields)
@@ -6676,24 +6677,16 @@ func (m *EncodeStats) UnmarshalVT(dAtA []byte) error {
 			iNdEx += 4
 			m.AvgFrameEncodeTimeMs = float32(math.Float32frombits(v))
 		case 4:
-			if wireType != 0 {
+			if wireType != 5 {
 				return fmt.Errorf("proto: wrong wireType = %d for field AvgFps", wireType)
 			}
-			m.AvgFps = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflow
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.AvgFps |= int64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
+			var v uint32
+			if (iNdEx + 4) > l {
+				return io.ErrUnexpectedEOF
 			}
+			v = uint32(binary.LittleEndian.Uint32(dAtA[iNdEx:]))
+			iNdEx += 4
+			m.AvgFps = float32(math.Float32frombits(v))
 		default:
 			iNdEx = preIndex
 			skippy, err := skip(dAtA[iNdEx:])
