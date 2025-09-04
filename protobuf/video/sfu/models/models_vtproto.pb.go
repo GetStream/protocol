f3c48065466d5552e63e9df6c810fe353499e0a7
guldev25 <gulzar.ahmed@getstream.io>
2025-09-04 11:31:02 +0200

    Revert "Add AudioBitrateType enum and field to TrackInfo"
    This reverts commit b061070bfdc2ae0a91f602da3180e8b871288df0.

--- protobuf/video/sfu/models/models_vtproto.pb.go
+++ protobuf/video/sfu/models/models_vtproto.pb.go
@@ -824,11 +824,6 @@ func (m *TrackInfo) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
-	if m.AudioBitrateType != 0 {
-		i = encodeVarint(dAtA, i, uint64(m.AudioBitrateType))
-		i--
-		dAtA[i] = 0x68
-	}
 	if m.PublishOptionId != 0 {
 		i = encodeVarint(dAtA, i, uint64(m.PublishOptionId))
 		i--
@@ -2139,9 +2134,6 @@ func (m *TrackInfo) SizeVT() (n int) {
 	}
 	if m.PublishOptionId != 0 {
 		n += 1 + sov(uint64(m.PublishOptionId))
-	}
-	if m.AudioBitrateType != 0 {
-		n += 1 + sov(uint64(m.AudioBitrateType))
 	}
 	if m.unknownFields != nil {
 		n += len(m.unknownFields)
@@ -4721,25 +4713,6 @@ func (m *TrackInfo) UnmarshalVT(dAtA []byte) error {
 				b := dAtA[iNdEx]
 				iNdEx++
 				m.PublishOptionId |= int32(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 13:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field AudioBitrateType", wireType)
-			}
-			m.AudioBitrateType = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflow
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.AudioBitrateType |= AudioBitrateType(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
